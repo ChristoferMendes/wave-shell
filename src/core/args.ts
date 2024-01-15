fe88b665d017ff777d59f7bd19dd0430591a73e5
Christofer <christoferluizdsm@gmail.com>
2024-01-15 11:37:01 -0300

    feat(args): add argument parsing function

---
+++ src/core/args.ts
@@ -0,0 +1,44 @@
+export interface WaveArguments {
+  [key: string]: boolean | string;
+}
+
+export function args() {
+  const args = process.argv.slice(2);
+  const commandName = args[0];
+
+  function isBooleanFlag(arg: string): boolean {
+    return arg.startsWith('-');
+  }
+
+  function parseStringValue(value: string): boolean | string {
+    return isBooleanFlag(value) ? true : value;
+  }
+
+  function parseArguments(): WaveArguments {
+    const commandArgs = args.slice(1);
+
+    return commandArgs.reduce((acc, arg, index) => {
+      if (arg.startsWith('--')) {
+        const nextArg = commandArgs[index + 1];
+        const key = arg.slice(2);
+
+        if (nextArg && !isBooleanFlag(nextArg)) {
+          return { ...acc, [key]: parseStringValue(nextArg) };
+        }
+
+        return { ...acc, [key]: true };
+      }
+
+      if (arg.startsWith('-')) {
+        return { ...acc, [arg.slice(1)]: true };
+      }
+
+      return acc;
+    }, {});
+  }
+
+  return {
+    commandName,
+    parsedArguments: parseArguments()
+  };
+}
