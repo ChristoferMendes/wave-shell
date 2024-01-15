ebeec26c909a21135c7a6e4c9cadea26bb2ea23a
Christofer <christoferluizdsm@gmail.com>
2024-01-15 19:52:15 -0300

    feat(types): add print option to WaveRunOptions

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
 import { WaveArguments } from "~/core/args.ts";
+import { WavePrint } from "./utils/print";
 
 export interface WaveRunOptions<Args> {
   args: WaveArguments & Args;
+  print: ReturnType<typeof WavePrint>;
 }
 
 export interface WaveCommand<Args = {}> {
