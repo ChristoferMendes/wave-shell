8669db5bcaed15c06cd16eebad96dfbedc74ae1b
Christofer <christoferluizdsm@gmail.com>
2024-01-15 11:37:08 -0300

    feat: add StringHelper class and compareTwoStrings method

---
+++ src/helpers/string.helper.ts
@@ -0,0 +1,54 @@
+export class StringHelper {
+  static findBestMatch(mainString: string, targetStrings: string[]) {
+    const ratings = [];
+    let bestMatchIndex = 0;
+
+    for (let i = 0; i < targetStrings.length; i++) {
+      const currentTargetString = targetStrings[i];
+      const currentRating = this.compareTwoStrings(mainString, currentTargetString)
+      ratings.push({target: currentTargetString, rating: currentRating})
+      if (currentRating > ratings[bestMatchIndex].rating) {
+        bestMatchIndex = i
+      }
+    }
+    
+    const bestMatch = ratings[bestMatchIndex]
+
+    return { ratings: ratings, bestMatch: bestMatch, bestMatchIndex: bestMatchIndex };
+  }
+
+  static compareTwoStrings(first: string, second: string) {
+    first = first.replace(/\s+/g, '')
+    second = second.replace(/\s+/g, '')
+
+    if (first === second) return 1;
+
+    if (first.length < 2 || second.length < 2) return 0;
+
+    let firstBigrams = new Map();
+    for (let i = 0; i < first.length - 1; i++) {
+      const bigram = first.substring(i, i + 2);
+      const count = firstBigrams.has(bigram)
+        ? firstBigrams.get(bigram) + 1
+        : 1;
+
+      firstBigrams.set(bigram, count);
+    }
+
+    let intersectionSize = 0;
+    for (let i = 0; i < second.length - 1; i++) {
+      const bigram = second.substring(i, i + 2);
+      const count = firstBigrams.has(bigram)
+        ? firstBigrams.get(bigram)
+        : 0;
+
+      if (count > 0) {
+        firstBigrams.set(bigram, count - 1);
+        intersectionSize++;
+      }
+    }
+
+    return (2.0 * intersectionSize) / (first.length + second.length - 2);
+  }
+
+}
