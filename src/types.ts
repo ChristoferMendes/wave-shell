d9193e429a5faa306cf310ad373e7cfda8e02d92
Christofer <christoferluizdsm@gmail.com>
2024-01-27 15:27:51 -0300

    refactor: Refactor WaveCommand interface and WaveRunOptions interface

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,28 @@
+import { z } from "zod";
 import { WaveArguments } from "./core/types";
 import { WavePrint } from "./utils/print";
-import { compileTemplate } from 'surfstar'
+import { compileTemplate } from "surfstar";
+import { prompt } from "./utils/prompt";
+
+export interface WaveCommand<Args = {}> {
+  argsSchema?: () => {
+    argsArraySchema?: z.ZodArray<any, any>;
+    namedArgsSchema?: z.ZodObject<any, any, any>;
+  };
+  run: (options: WaveRunOptions<Args>) => Promise<void>;
+  description?: string;
+}
 
 
-export interface WaveRunOptions<Args> {
-  args: WaveArguments & Args;
+export interface WaveRunOptions<ArgsT = {}> {
+  args: {
+    argsArray: string[];
+    namedArgs: WaveArguments['namedArgs'] & ArgsT;
+  };
   print: ReturnType<typeof WavePrint>;
+  prompt: typeof prompt;
   compileTemplate: typeof compileTemplate;
 }
 
-export type ValidateErrorMessage = {
-  message: string;
-}
-
-export interface WaveCommand<Args = {}> {
-  run: (options: WaveRunOptions<Args>) => Promise<void>;
-  description?: string;
-  validateArgs?: (args: WaveArguments & Args) => ValidateErrorMessage;
-}
-
-export * from '~/utils/types'
-export * from '~/core/types'
+export * from "~/utils/types";
+export * from "~/core/types";
